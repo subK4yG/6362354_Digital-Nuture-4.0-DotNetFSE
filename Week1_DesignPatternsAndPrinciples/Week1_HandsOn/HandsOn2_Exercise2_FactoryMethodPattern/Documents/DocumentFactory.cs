b337a27b8d44435e9266debfed1030326e9a9a17
subK4yG <subhasmitakhuntia14@gmail.com>
2025-06-22 15:27:22 +0530

    Add Week1 hands-on
    Design Patterns and Principles

--- Week1_DesignPatternsAndPrinciples/Week1_HandsOn/HandsOn2_Exercise2_FactoryMethodPattern/Documents/DocumentFactory.cs
+++
@@ -1,7 +0,0 @@
-namespace HandsOn2_Exercise2_FactoryMethodPattern.Documents
-{
-    public abstract class DocumentFactory
-    {
-        public abstract IDocument CreateDocument();
-    }
-}
--- Week1_DesignPatternsAndPrinciples/Week1_HandsOn/HandsOn2_Exercise2_FactoryMethodPattern/Documents/IDocument.cs
+++
@@ -1,7 +0,0 @@
-namespace HandsOn2_Exercise2_FactoryMethodPattern.Documents
-{
-    public interface IDocument
-    {
-        void Open();
-    }
-}
