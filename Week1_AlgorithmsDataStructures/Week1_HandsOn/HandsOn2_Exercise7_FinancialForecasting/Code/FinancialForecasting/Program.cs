90379bcac29cb166cb86d14f2c427b21f70782e8
subK4yG <subhasmitakhuntia14@gmail.com>
2025-06-22 23:25:43 +0530

    Add Week1 hands-on
    Algorithms Data Structures

---
+++ Week1_AlgorithmsDataStructures/Week1_HandsOn/HandsOn2_Exercise7_FinancialForecasting/Code/FinancialForecasting/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace FinancialForecasting
+{
+    class Program
+    {
+        static double PredictFutureValue(double current, double rate, int years)
+        {
+            if (years == 0)
+                return current;
+            return PredictFutureValue(current * (1 + rate), rate, years - 1);
+        }
+        static void Main()
+        {
+            double presentValue = 10000;
+            double growthRate = 0.10;
+            int years = 5;
+            double futureValue = PredictFutureValue(presentValue, growthRate, years);
+            Console.WriteLine("Future Value after " + years + " years: Rs." + futureValue);
+        }
+    }
+}
